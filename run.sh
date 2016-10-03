e248f8cf4fb6dc581d419f577fe614321dbdd31a
Anthony Green <akoaysigod@gmail.com>
2016-10-02 20:58:19 -0400

    update run doesnt help

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-./.build/release/RayTracingWeekend 1 4 > p1.ppm& 
-./.build/release/RayTracingWeekend 2 4 > p2.ppm&
-./.build/release/RayTracingWeekend 3 4 > p3.ppm&
-./.build/release/RayTracingWeekend 4 4 > p4.ppm&
+
+for i in {1..20}
+do
+  ./.build/release/RayTracingWeekend $i 20 > p$i.ppm& 
+done
 wait
+
 cat p1.ppm > test.ppm 
-cat p2.ppm >> test.ppm 
-cat p3.ppm >> test.ppm 
-cat p4.ppm >> test.ppm 
+for i in {1..20}
+do 
+  cat p$i.ppm >> test.ppm 
+done
