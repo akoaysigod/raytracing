13b17ef66e6d9bed0986dd291bf86b498778d73c
Anthony Green <akoaysigod@gmail.com>
2016-12-04 21:00:46 -0500

    hmm

--- Sources/Perlin.swift
+++ Sources/Perlin.swift
@@ -1,21 +1,21 @@
 import Foundation
 
 final class Perlin {
-  private let randomDoubles: [Double]
+  private let randomVectors: [Vector]
   private let xPerms: [Int]
   private let yPerms: [Int]
   private let zPerms: [Int]
 
   init() {
-    randomDoubles = Perlin.generateRandomDoubles()
+    randomVectors = Perlin.generateRandomVectors()
     xPerms = Perlin.generatePermutations()
     yPerms = Perlin.generatePermutations()
     zPerms = Perlin.generatePermutations()
   }
 
-  func trilinearInterpolation(randDoubles: [[[Double]]], u: Double, v: Double, w: Double) -> Double {
+  func trilinearInterpolation(randVectors: [[[Vector]]], u: Double, v: Double, w: Double) -> Double {
     func hermiteCubic(_ x: Double) -> Double {
-      return x * x * (3 - 2 * x)
+      return x * x * (3.0 - 2.0 * x)
     }
 
     let uu = hermiteCubic(u)
@@ -25,10 +25,11 @@ final class Perlin {
     return (0..<2).reduce(0.0) { (ret, i) in
       return (0..<2).reduce(ret) { (ret1, j) in
         return (0..<2).reduce(ret1) { (ret2, k) in
-          let a = Double(i) * uu + Double(1 - i) * (1 - uu)
-          let b = Double(j) * vv + Double(1 - j) * (1 - vv)
-          let c = Double(k) * ww + Double(1 - k) * (1 - ww)
-          return ret2 + a * b * c * randDoubles[i][j][k]
+          let weight = Vector(u - Double(i), v - Double(j), w - Double(k))
+          let a = Double(i) * uu + Double(1 - i) * (1.0 - uu)
+          let b = Double(j) * vv + Double(1 - j) * (1.0 - vv)
+          let c = Double(k) * ww + Double(1 - k) * (1.0 - ww)
+          return ret2 + a * b * c * randVectors[i][j][k].dotp(weight)
         }
       }
     }
@@ -39,13 +40,13 @@ final class Perlin {
     let j = Int(vec.y)
     let k = Int(vec.z)
 
-    let randDoubles = (0..<2).map { (di) -> [[Double]] in
+    let randVectors = (0..<2).map { (di) -> [[Vector]] in
       return (0..<2).map { dj in
         return (0..<2).map { dk in
           let x = xPerms[(i + di) & 255]
           let y = yPerms[(j + dj) & 255]
           let z = zPerms[(k + dk) & 255]
-          return randomDoubles[x ^ y ^ z]
+          return randomVectors[x ^ y ^ z]
         }
       }
     }
@@ -53,11 +54,14 @@ final class Perlin {
     let u = vec.x - floor(vec.x)
     let v = vec.y - floor(vec.y)
     let w = vec.z - floor(vec.z)
-    return trilinearInterpolation(randDoubles: randDoubles, u: u, v: v, w: w)
+    return trilinearInterpolation(randVectors: randVectors, u: u, v: v, w: w)
   }
 
-  private static func generateRandomDoubles() -> [Double] {
-    return (0..<256).map { _ in drand48() }
+  private static func generateRandomVectors() -> [Vector] {
+    return (0..<256).map { _ in
+      let arr = (0..<3).map { _ in -1.0 + 2.0 * drand48() }
+      return Vector(array: arr).unit
+    }
   }
 
   private static func generatePermutations() -> [Int] {
